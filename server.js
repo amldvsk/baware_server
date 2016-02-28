da96178b97a604a8788db9a15d7cd6947ffde510
adi moldavski <adi@active-digital.col.il>
2016-02-28 14:46:47 +0200

    sdf

--- server.js
+++ server.js
@@ -8,39 +8,44 @@ mongoose = mongoose.connect('mongodb://localhost/baware');
 
 var app = express();
 
+app.configure(function(){
+    app.set('views', './public/views');
+    app.set('view engine', 'jade');
 
-app.use(function (req, res, next) {
+    app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
-});
+        // Pass to next layer of middleware
+        next();
+    });
 
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(bodyParser.json({limit: '2mb'}));
-app.use(bodyParser.urlencoded({limit: '2mb', extended : true}));
-app.use(morgan('dev'));
-
-
+    app.use(express.static(__dirname + '/public'));
+    app.use(bodyParser.json({limit: '2mb'}));
+    app.use(bodyParser.urlencoded({limit: '2mb', extended : true}));
+    app.use(morgan('dev'));
 
 
 
-app.set('views', './public/views');
-app.set('view engine', 'jade');
 
-app.use('/api', require('./routes/api'));
+
+
+
+    app.use('/api', require('./routes/api'));
+
+})
+
 
 
 app.get('/', function(req, res) {
