1715863a6c38a02577007fd8711e245019130a95
adimoldavski <amldvsk@gmail.com>
2016-02-28 19:47:46 +0200

    baware

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use(express.static(__dirname + '/public'));
+app.use('/static', express.static(__dirname + '/public'));
 app.use(bodyParser.json({limit: '2mb'}));
 app.use(bodyParser.urlencoded({limit: '2mb', extended : true}));
 app.use(morgan('dev'));
